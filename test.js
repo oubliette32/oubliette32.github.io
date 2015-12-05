1138fa236c2bc4d725c53bad901f0a0b0c9b8c81
defc0n <contact@hexisoft.net>
2015-12-05 20:13:38 +0100

    trying to circumvent access origin headers

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 var xhttp = new XMLHttpRequest();
 
+document.domain = "urbangamers.net"
+
 xhttp.open("POST", "http://urbangamers.net/thumbs.php", true);
 xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 xhttp.send("thumbs.php?do=add&contenttype=post&thumb=-1&contentid=261919&securitytoken=1449342463-db70592f86bed575d0829051e5a152c82bdb8110");
